e5410cb662596128a7e179da44f8cf39e28a4ed4
myron6520 <100897740+myron6520@users.noreply.github.com>
2022-03-25 12:22:53 +0800

    Update wallet_util_test.dart

--- wallet/packages/wallet_util/test/wallet_util_test.dart
+++ wallet/packages/wallet_util/test/wallet_util_test.dart
@@ -5,15 +5,4 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wallet_util/wallet_util.dart';
 import 'package:bip39/bip39.dart';
 
-void main() {
-  String mnemonic = generateMnemonic();
-  Uint8List seed = mnemonicToSeed(
-      "process forget believe wealth tennis ski radio coral swim home clay topple");
-  HDWallet wallet = HDWallet(seed: seed);
-  print("mnemonic:$mnemonic");
-  print("ETH:${wallet.p2shAddr}");
-  print("ETH:${wallet.legacyAddr}");
-  print("ETH:${wallet.base32Addr}");
-  //0x0D1cb3853A2e654B159E615Db8E9E01E32901b19
-  //0x45aF1F12CD91b0648D27467d0CE599e72292edB4
-}
+void main() {}
